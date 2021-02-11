88dd6095027a2fc4c722d8da08a07c24ec6d7e20
geolffrey <gmjun2000@gmail.com>
2021-02-11 11:48:09 -0600

    Remove section from landing

--- src/containers/dashboard/index.jsx
+++ src/containers/dashboard/index.jsx
@@ -59,69 +59,6 @@ export const Dashboard = () => {
                     </ImageWrapper>
                 </ImagesContainer>
             </Section>
-            <Section>
-                <Title>Getting started</Title>
-                <SubTitle>
-                    Setup
-                </SubTitle>
-                <TextContent>
-                    Please run <strong>npm i</strong> to install dependencies and add .env file variables:<br/><br/>
-
-                    BROWSER=none
-                </TextContent>
-                <SubTitle>
-                    Available Scripts
-                </SubTitle>
-                <TextContent>
-                    In the project directory, you can run:
-                </TextContent>
-                <Command>
-                    npm run dev
-                </Command>
-                <TextContent>
-                    Runs the app in the development mode.<br/>
-                    Open to view it in electron.
-                </TextContent>
-                <Command>
-                    npm run inspect
-                </Command>
-                <TextContent>
-                    Runs the app in the development mode.<br/>
-                    Open to view it in electron with inspect mode to connect to chrome-inspector.
-                </TextContent>
-                <Command>
-                    npm run build
-                </Command>
-                <TextContent>
-                    Builds the app for production to the build folder.<br/>
-                    It correctly bundles React in production mode and optimizes the build for the best performance.<br/>
-                    The build is minified and the filenames include the hashes.<br/>
-                    Your app is ready to be deployed!<br/><br/>
-
-                    See the section about deployment for more information.
-                </TextContent>
-                <Command>
-                    npm run eject
-                </Command>
-                <TextContent>
-                    <strong>Note: this is a one-way operation. Once you eject, you can’t go back!</strong><br/>
-
-                    If you aren’t satisfied with the build tool and configuration choices, you can eject at any time. This command will remove the single build dependency from your project.<br/>
-
-                    Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except eject will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.<br/>
-
-                    You don’t have to ever use eject. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.<br/>
-                </TextContent>
-                <SubTitle>
-                    Packaging
-                </SubTitle>
-                <Command>
-                    npm run package-os
-                </Command>
-                <TextContent>
-                    Build app to choosen OS. Depending on packaging OS will need pack in respective OS.
-                </TextContent>
-            </Section>
         </Content>
         <Footer>© 2020 ZorrillosDev Community.</Footer>
       </DashboardContainer>
