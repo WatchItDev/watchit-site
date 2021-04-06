18a608d92c91d8cda9e01d75b6e5ceaef4b701a7
jadapema <Ruperma040193>
2021-04-06 11:58:07 -0600

    feat: add contributors

---
+++ src/containers/dashboard/contributors.js
@@ -0,0 +1,87 @@
+export default {
+    "contributors": [
+        {
+            "login": "geolffreym",
+            "name": "Geolffrey Mena",
+            "avatar_url": "https://avatars.githubusercontent.com/u/8823234?v=4",
+            "profile": "https://github.com/geolffreym",
+            "contributions": [
+                "mentoring",
+                "ideas",
+                "code",
+                "doc",
+                "review",
+                "userTesting",
+                "infra",
+            ]
+        },
+        {
+            "login": "xirapo",
+            "name": "FranciscoGarcia",
+            "avatar_url": "https://avatars.githubusercontent.com/u/12839111?v=4",
+            "profile": "https://github.com/xirapo",
+            "contributions": [
+                "doc",
+                "code"
+            ]
+        },
+        {
+            "login": "jadapema",
+            "name": "Jacob Peralta",
+            "avatar_url": "https://avatars.githubusercontent.com/u/14361925?v=4",
+            "profile": "https://github.com/Jadapema",
+            "contributions": [
+                "code",
+                "design",
+                "blog",
+                "ideas"
+            ]
+        },
+        {
+            "login": "aphelionz",
+            "name": "Mark Robert Henderson",
+            "avatar_url": "https://avatars.githubusercontent.com/u/106148?v=4",
+            "profile": "http://mrh.io",
+            "contributions": [
+                "mentoring",
+                "ideas",
+                "code",
+                "review",
+                "userTesting"
+            ]
+        },
+        {
+            "login": "vaultec",
+            "name": "vaultec",
+            "avatar_url": "https://avatars.githubusercontent.com/u/47548474?v=4",
+            "profile": "https://github.com/vaultec81",
+            "contributions": [
+                "ideas",
+                "userTesting",
+                "talks",
+            ]
+        },
+        {
+            "login": "phillmac",
+            "name": "phillmac",
+            "avatar_url": "https://avatars.githubusercontent.com/u/4534835?v=4",
+            "profile": "https://github.com/phillmac",
+            "contributions": [
+                "ideas",
+                "talks",
+                "code",
+                "userTesting",
+                "infra"
+            ]
+        },
+        {
+            "login": "vasa",
+            "name": "vasa",
+            "avatar_url": "https://avatars.githubusercontent.com/u/28847087?v=4",
+            "profile": "https://vaibhavsaini.com/",
+            "contributions": [
+                "review"
+            ]
+        },
+    ]
+}
