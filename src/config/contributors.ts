15b4bcdabcc2b8811a4bee89762eee2702bb48a6
geolffreym <gmjun2000@gmail.com>
2022-06-27 10:39:49 -0600

    c

--- src/config/contributors.ts
+++ src/config/contributors.ts
@@ -1,7 +1,7 @@
-import Alejandro from '@assets/img/alejandro.png'
+// import Alejandro from '@assets/img/alejandro.png'
 import Jadapema from '@assets/img/jadapema.png'
-import Phillip from '@assets/img/phillip.png'
-import Frank from '@assets/img/frank.png'
+// import Phillip from '@assets/img/phillip.png'
+// import Frank from '@assets/img/frank.png'
 import Mark from '@assets/img/mark.png'
 import Geo from '@assets/img/geo.png'
 import Denis from '@assets/img/denis.jpeg'
@@ -17,34 +17,16 @@ export const CONTRIBUTIONS = {
             "img": Geo
         },
         {
-            "name": "Mark Henderson",
-            "role": "Co-Founder / Strategy Advisor",
-            "profile": "http://mrh.io",
-            "img": Mark
-        },
-        {
             "name": "Jacob Peralta",
             "role": "Co-Founder / Front-End Lead",
             "profile": "https://github.com/Jadapema",
             "img": Jadapema
         },
         {
-            "name": "Francisco Garcia",
-            "role": "Project Manager",
-            "profile": "https://github.com/xirapo",
-            "img": Frank
-        },
-        {
-            "name": "Phillip Mackintosh",
-            "role": "Architecture Advisor",
-            "profile": "https://github.com/phillmac",
-            "img": Phillip
-        },
-        {
-            "name": "Alejandro González",
-            "role": "Front-End Developer",
-            "profile": "https://github.com/JoseGonz98",
-            "img": Alejandro
+            "name": "Mark Henderson",
+            "role": "Strategy Advisor",
+            "profile": "http://mrh.io",
+            "img": Mark
         },
         {
             "name": "Dennis",
