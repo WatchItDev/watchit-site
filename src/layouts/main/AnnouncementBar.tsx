9f7685bb11a24e5f782c4cc2631112975a66c8ea
jadapema <jadapema@gmail.com>
2024-12-13 13:39:58 -0600

    feat: added watchit beta live announcement bar

--- src/layouts/main/AnnouncementBar.tsx
+++ src/layouts/main/AnnouncementBar.tsx
@@ -1,13 +1,33 @@
 import { Box, Container, Typography } from '@mui/material';
+import AppBar from "@mui/material/AppBar";
+import Link from "@mui/material/Link";
+import HideOnScroll from "../../components/hide-on-scroll";
 
 export default function AnnouncementBar() {
     return (
-        <Box sx={{ width: '100%', bgcolor: 'info.main', p: 1 }}>
-            <Container>
-                <Typography variant="body2" color="#fff" sx={{ textAlign: 'center', fontWeight: 'bold' }}>
-                    @everyone We’re thrilled to announce the next big steps for Watchit! 🚀 Our Web3-based platform is almost ready...
-                </Typography>
-            </Container>
-        </Box>
+        <HideOnScroll>
+            <AppBar
+                position="static"
+                sx={{
+                    background: 'linear-gradient(90deg,#13a5d3,#1b6bec 32.29%,#9824cf)!important',
+                    zIndex: 1,
+                    p: 0.5
+                }}
+            >
+                <Container>
+                    <Typography variant="body2" sx={{ color: '#fff', textAlign: 'center' }}>
+                        🚀 The Watchit Public Beta is now live! 🎉 {' '} {' '}
+                        <Link
+                            href="https://app.watchit.movie/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            sx={{ textDecoration: 'underline', color: '#fff', fontWeight: 'bold' }}
+                        >
+                            Click here to launch the app.
+                        </Link>
+                    </Typography>
+                </Container>
+            </AppBar>
+        </HideOnScroll>
     );
 }
