782d4229765cb61aef4db0f7640e5a0ea95e726e
Geolffrey Mena <gmjun2000@gmail.com>
2025-01-09 13:06:48 -0600

    chore: improved title

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
   return (
     <>
       <Helmet>
-        <title>Watchit: A New Era of Video Content with Web3 x AI</title>
+        <title>Watchit: The Future of Video Content Powered by Web3 and AI</title>
       </Helmet>
 
       <HomeView />
